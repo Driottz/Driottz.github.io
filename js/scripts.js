0f8b84fa45ca495478b17cd7be48d28174bf1333
Norman Kevin Driottz <nkdriottz@gmail.com>
2021-09-23 13:31:30 -0700

    rev 6

--- js/scripts.js
+++ js/scripts.js
@@ -56,29 +56,18 @@ window.addEventListener('DOMContentLoaded', event => {
     })
 })
 
-function fadeOut(el) {
-    el.style.opacity = 1;
-    (function fade() {
-        if ((el.style.opacity -= .1) < 0) {
-            el.style.display = "none";
-        } else {
-            requestAnimationFrame(fade);
-        }
-    })();
-};
-
-function fadeIn(el, display) {
-    el.style.opacity = 0;
-    el.style.display = display || "block";
-    (function fade() {
-        var val = parseFloat(el.style.opacity);
-        if (!((val += .1) > 1)) {
-            el.style.opacity = val;
-            requestAnimationFrame(fade);
-        }
-    })();
-};
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+          openEffect: "none",
+          closeEffect: "none"
+      });
+      
+      $(".zoom").hover(function(){
+          
+          $(this).addClass('transition');
+      }, function(){
+          
+          $(this).removeClass('transition');
+      });
+  });
+      
